95e4611696841ddecf22a0d5c06d85c5230b3cf9
Hemang Joshi <hemangjoshi37a@gmail.com>
2023-01-21 01:56:26 +0530

    Automatic commit

--- execute_all.py
+++ execute_all.py
@@ -4,11 +4,7 @@ import subprocess
 subprocess.call(["python", "rename.py"])
 
 # Execute the html_to_json.py script
-<<<<<<< HEAD
 # subprocess.call(["python", "html_to_json.py"])
-=======
-subprocess.call(["python", "html_to_json.py"])
->>>>>>> 2b5cc6ea0947a8191027d5c0ca7e481f113e6eb6
 
 # Execute the git_automate.py script
 subprocess.call(["python", "git_automate.py"])
